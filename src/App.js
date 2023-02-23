81f8ed40f4c5731734e513b4be3bc8876c463e15
AhmadIznaurov <Ahmad.iznaurov@mail.ru>
2023-02-23 23:23:30 +0300

    footer adding

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Header />
         <Navbar />
         <Profile />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div><a href="#">Next page</a></div>
+            <div><a href="#">Advice for learners</a></div>
+            <div><a href="#">Blog</a></div>
+            <div><a href="#">Info</a></div>
+        </footer>
+    )
+}
+
+export default Footer;
